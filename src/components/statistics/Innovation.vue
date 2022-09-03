bb6a6eaea77b319d2ca2447094d53bbb4970e33d
Memory Wang <memory5776@gmail.com>
2022-09-04 03:34:05 +0800

    add test data for layout.

--- src/components/statistics/Innovation.vue
+++ src/components/statistics/Innovation.vue
@@ -2,29 +2,37 @@
   <section>
     <p class="other_subtitle">國內外專利</p>
     <section class="container_grid">
-      <PieChart />
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart title="" :data="data.pieTest"/>
     </section>
     <p class="other_subtitle">國際認證</p>
     <section class="container_grid">
-      <PieChart />
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart title="" :data="data.pieTest"/>
     </section>
     <p class="other_subtitle">創新商業化</p>
     <section class="container_grid">
-      <PieChart />
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart title="" :data="data.pieTest"/>
     </section>
   </section>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 import PieChart from "@/components/chart/PieChart.vue";
 
 export default defineComponent({
   components: { PieChart },
+  //TODO
   setup() {
-    return {};
+    const data = reactive({
+      pieTest: [
+        { label: "總資產", value: 0 }, 
+        { label: "股東權益", value: 0 },
+        { label: "總負債", value: 0 },
+      ],
+    })
+    return {data};
   },
 });
 </script>
--- src/components/statistics/Market.vue
+++ src/components/statistics/Market.vue
@@ -2,30 +2,38 @@
   <section>
     <p class="other_subtitle">國際供應鏈關鍵地位</p>
     <section class="container_grid">
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
     </section>
     <p class="other_subtitle">自有品牌海外出貨比例</p>
     <section class="container_grid">
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
     </section>
     <p class="other_subtitle">淨零碳排落實狀況</p>
     <section class="container_grid">
-      <PieChart />
-      <PieChart />
-      <PieChart />
-      <PieChart />
-      <PieChart />
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart type="2" title="" :data="data.pieTest"/>
+      <PieChart type="2" title="" :data="data.pieTest"/>
     </section>
   </section>
 </template>
 <script lang="ts">
-import { defineComponent } from "vue";
+import { defineComponent, reactive } from "vue";
 import PieChart from "@/components/chart/PieChart.vue";
 
 export default defineComponent({
   components: { PieChart },
+  //TODO
   setup() {
-    return {};
+    const data = reactive({
+      pieTest: [
+        { label: "總資產", value: 0 }, 
+        { label: "股東權益", value: 0 },
+        { label: "總負債", value: 0 },
+      ],
+    })
+    return {data};
   },
 });
 </script>
