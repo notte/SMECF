44aaa3c85c162b6ddd1db023a828b8777758c7b4
Memory Wang <memory5776@gmail.com>
2022-09-10 15:26:50 +0800

    rename Nav.vue file name.

--- src/components/dashboard/Base/Base.vue
+++ src/components/dashboard/Base/Base.vue
@@ -76,42 +76,46 @@ export default defineComponent({
             { name: "財務力", data: res.data.eachDimensionAvgScore.F, max: 10, color: "purple" },
             { name: "數位力", data: res.data.eachDimensionAvgScore.I, max: 10, color: "blue" },
             { name: "創新力", data: res.data.eachDimensionAvgScore.N, max: 10, color: "green" },
-            { name: "市場力", data: res.data.eachDimensionAvgScore.D, max: 10, color: "orange" }
+            { name: "市場力", data: res.data.eachDimensionAvgScore.D, max: 10, color: "orange" },
           ];
           data.highlightCompany = [
             { name: "公司 A", data: res.data.highlightCompany[1], max: 10 },
             { name: "公司 B", data: res.data.highlightCompany[2], max: 10 },
             { name: "公司 C", data: res.data.highlightCompany[3], max: 10 },
             { name: "公司 D", data: res.data.highlightCompany[4], max: 10 },
-            { name: "公司 E", data: res.data.highlightCompany[5], max: 10 }
+            { name: "公司 E", data: res.data.highlightCompany[5], max: 10 },
           ];
           data.manufacturer = [
-            { name: "公司年資", data: [
-              { label: "5年以下", value: res.data.manufacturer.date[1] },
-              { label: "6-20年", value: res.data.manufacturer.date[2]  },
-              { label: "21-50年", value: res.data.manufacturer.date[3]  },
-              { label: "51年以上", value: res.data.manufacturer.date[4]  }]
-            },
-            { name: "資本額", data: [
-              { label: "2億以上", value: res.data.manufacturer.capital[1] },
-              { label: "1億-2億", value: res.data.manufacturer.capital[2] },
-              { label: "7500萬-1億", value: res.data.manufacturer.capital[3] },
-              { label: "5000萬-7500萬", value: res.data.manufacturer.capital[4] },
-              { label: "2500萬-5000萬", value: res.data.manufacturer.capital[5] },
-              { label: "1000萬-2500萬", value: res.data.manufacturer.capital[6] },
-              { label: "1000萬以下", value: res.data.manufacturer.capital[7] },
+            { 
+              name: "公司年資", data: [
+                { label: "5年以下", value: res.data.manufacturer.date[1] },
+                { label: "6-20年", value: res.data.manufacturer.date[2] },
+                { label: "21-50年", value: res.data.manufacturer.date[3] },
+                { label: "51年以上", value: res.data.manufacturer.date[4] },
             ]},
-            { name: "員工數", data: [
-              { label: "10人以下", value: res.data.manufacturer.sumPeople[1] },
-              { label: "11-100人", value: res.data.manufacturer.sumPeople[2] },
-              { label: "101-200人", value: res.data.manufacturer.sumPeople[3] },
-              { label: "201人以上", value: res.data.manufacturer.sumPeople[4] }
+            { 
+              name: "資本額", data: [
+                { label: "2億以上", value: res.data.manufacturer.capital[1] },
+                { label: "1億-2億", value: res.data.manufacturer.capital[2] },
+                { label: "7500萬-1億", value: res.data.manufacturer.capital[3] },
+                { label: "5000萬-7500萬", value: res.data.manufacturer.capital[4] },
+                { label: "2500萬-5000萬", value: res.data.manufacturer.capital[5] },
+                { label: "1000萬-2500萬", value: res.data.manufacturer.capital[6] },
+                { label: "1000萬以下", value: res.data.manufacturer.capital[7] },
             ]},
-            { name: "場址地區", data: [
-              { label: "東", value: res.data.manufacturer.st[1] },
-              { label: "南", value: res.data.manufacturer.st[2] },
-              { label: "西", value: res.data.manufacturer.st[3] },
-              { label: "北", value: res.data.manufacturer.st[4] }
+            { 
+              name: "員工數", data: [
+                { label: "10人以下", value: res.data.manufacturer.sumPeople[1] },
+                { label: "11-100人", value: res.data.manufacturer.sumPeople[2] },
+                { label: "101-200人", value: res.data.manufacturer.sumPeople[3] },
+                { label: "201人以上", value: res.data.manufacturer.sumPeople[4] },
+            ]},
+            { 
+              name: "場址地區", data: [
+                { label: "東", value: res.data.manufacturer.st[1] },
+                { label: "南", value: res.data.manufacturer.st[2] },
+                { label: "西", value: res.data.manufacturer.st[3] },
+                { label: "北", value: res.data.manufacturer.st[4] },
             ]},
           ];
         })
