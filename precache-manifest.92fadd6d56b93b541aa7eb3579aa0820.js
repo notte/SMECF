50f602e2af4b463b595acc911d52b5bfdafffb4a
Yoyo <maonome@gmail.com>
2022-12-02 11:47:30 +0800

    Updates

---
+++ precache-manifest.92fadd6d56b93b541aa7eb3579aa0820.js
@@ -0,0 +1,142 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c959c51e1247ff9afb262e9c3f98320",
+    "url": "/smecf/Taiwan.geo.json"
+  },
+  {
+    "revision": "b8db7c02c7ecbde8ad9f",
+    "url": "/smecf/css/app.a776c25f.css"
+  },
+  {
+    "revision": "9d855c26f41baa5d4d49",
+    "url": "/smecf/css/chunk-vendors.d4455c46.css"
+  },
+  {
+    "revision": "5556cdcc0855f4fd2a7df503d1525d4e",
+    "url": "/smecf/data/dashboard_basic.json"
+  },
+  {
+    "revision": "815435731114a5519af4e641dc3ab3d6",
+    "url": "/smecf/data/dashboard_other_develop.json"
+  },
+  {
+    "revision": "bac4187bde8d560b9c4149cdec8544b4",
+    "url": "/smecf/data/dashboard_other_finance.json"
+  },
+  {
+    "revision": "9773d75b0cf8fd8d2eb07e4c8b25842d",
+    "url": "/smecf/data/dashboard_other_intelligent.json"
+  },
+  {
+    "revision": "f4d8e1a0e3b981a9650c80b2c9d12d71",
+    "url": "/smecf/data/dashboard_other_new.json"
+  },
+  {
+    "revision": "ba0a9bc74737aef662b0ca00a9ddcb83",
+    "url": "/smecf/data/develop_statistics.json"
+  },
+  {
+    "revision": "869ed055383b27e661fe40e02bb8787c",
+    "url": "/smecf/data/finance_statistics.json"
+  },
+  {
+    "revision": "54df2b55500f6388f6f979e8542aaec3",
+    "url": "/smecf/data/intelligent_statistics.json"
+  },
+  {
+    "revision": "8298658ce9f1840efd01755f30b6b422",
+    "url": "/smecf/data/intelligent_statistics_detail.json"
+  },
+  {
+    "revision": "997af183ad294800febcb82cc2792bf9",
+    "url": "/smecf/data/manufacturer_statistics.json"
+  },
+  {
+    "revision": "72a54f7170ad9906c417a7f6b3a52ea5",
+    "url": "/smecf/data/manufacturer_statistics_detail.json"
+  },
+  {
+    "revision": "2f280c0aa3975f90d60658176ce1ef1f",
+    "url": "/smecf/data/manufacturer_statistics_region.json"
+  },
+  {
+    "revision": "da9d90c3db04a714cf8b28d1ce9b2be4",
+    "url": "/smecf/data/new_statistics.json"
+  },
+  {
+    "revision": "402efb19208a987946155a4c9df903a5",
+    "url": "/smecf/img/arrow_left.402efb19.svg"
+  },
+  {
+    "revision": "8b2a355a76fb25974f7c85cb75440227",
+    "url": "/smecf/img/arrow_left_dark.8b2a355a.svg"
+  },
+  {
+    "revision": "0098426ef115b87dcf18c8d0bc79ae91",
+    "url": "/smecf/img/chart.0098426e.svg"
+  },
+  {
+    "revision": "ee20d353f539c1cd6b0700b02fab5abc",
+    "url": "/smecf/img/chart_focus.ee20d353.svg"
+  },
+  {
+    "revision": "02a62820fbdd970ac4291a94aa6a9ae5",
+    "url": "/smecf/img/chart_mobile.02a62820.svg"
+  },
+  {
+    "revision": "2b43ea653305a160cb10c50bff6e758b",
+    "url": "/smecf/img/dashboard.2b43ea65.svg"
+  },
+  {
+    "revision": "1039e2d172b3b6fd8c8dbcf93358b63c",
+    "url": "/smecf/img/dashboard_focus.1039e2d1.svg"
+  },
+  {
+    "revision": "6b01ab9a489e461196f438e82e939e51",
+    "url": "/smecf/img/dashboard_mobile.6b01ab9a.svg"
+  },
+  {
+    "revision": "46caa6202ff93e3b001bff264a1e2942",
+    "url": "/smecf/img/moon_mobile.46caa620.svg"
+  },
+  {
+    "revision": "5fd0037912294d7e4596c82f8e010650",
+    "url": "/smecf/img/sun_mobile.5fd00379.svg"
+  },
+  {
+    "revision": "163a342b5ce239192f42787128fc27d6",
+    "url": "/smecf/img/table.163a342b.svg"
+  },
+  {
+    "revision": "3a8267db8305cec54656d7d80ba431c4",
+    "url": "/smecf/index.html"
+  },
+  {
+    "revision": "b8db7c02c7ecbde8ad9f",
+    "url": "/smecf/js/app.ae9d030d.js"
+  },
+  {
+    "revision": "21a847ce6367643bef54",
+    "url": "/smecf/js/chunk-18612721.a218bab1.js"
+  },
+  {
+    "revision": "19722303c8bc4e4c1c0e",
+    "url": "/smecf/js/chunk-1a125a83.0afc1f95.js"
+  },
+  {
+    "revision": "54d23543dbf39416fe58",
+    "url": "/smecf/js/chunk-e95528fc.9312f705.js"
+  },
+  {
+    "revision": "9d855c26f41baa5d4d49",
+    "url": "/smecf/js/chunk-vendors.420af4ed.js"
+  },
+  {
+    "revision": "1968e3974ce6585902ca4cdd8160c0e3",
+    "url": "/smecf/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/smecf/robots.txt"
+  }
+]);
